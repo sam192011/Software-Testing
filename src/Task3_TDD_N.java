dc79eb44af1dcd231d67557c6a003604a3cf8294
Jay Yeung <jay.yeung148@gmail.com>
2019-03-28 23:33:28 +0000

    Task2 FINISHED

--- src/Task3_TDD_N.java
+++ src/Task3_TDD_N.java
@@ -7,39 +7,53 @@ import st.Parser;
 import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 public class Task3_TDD_N {
 	
 	private Parser parser;
 
-	@Before
-	public void set_up() {
-		parser = new Parser();
-	}
-
-	@Test
-	public void BasicTest() {
-		parser.add("list", "l", Parser.STRING);
-		parser.parse("--list=1,2,4-7,10");
-		List 1 = parser.getIntegerList("list");
-		assertEquals(l, {1,2,4,5,6,7,10});
-	}
-
-	@Test
-	public void SearchOrderTest() {
-		parser.add("o", Parser.STRING);
-		parser.add("option", "o", Parser.STRING);
-		parser.parse("--o 1,2,3,4,5");
-		parser.parse("-o 6,7,8,9,10");
-		List 1 = parser.getIntegerList("o");
-		assertEquals(l, {1,2,3,4,5});
-	}
-	
-	@Test
-	public void NoValueTest() {
-		parser.add("list", "l", Parser.STRING);
-		parser.parse("--list");
-		List 1 = parser.getIntegerList("o");
-		assertEquals(l, []);
-	}
+//	@Before
+//	public void set_up() {
+//		parser = new Parser();
+//	}
+//
+//	@Test
+//	public void BasicTest() {
+//		parser.add("list", "l", Parser.STRING);
+//		parser.parse("--list=1,2,4-7,10");
+//		List l = parser.getIntegerList("list");
+//		assertEquals(l, [1,2,4,5,6,7,10]);
+//	}
+//
+//	@Test
+//	public void SearchOrderTest() {
+//		parser.add("o", Parser.STRING);
+//		parser.add("option", "o", Parser.STRING);
+//		parser.parse("--o 1,2,3,4,5");
+//		parser.parse("-o 6,7,8,9,10");
+//		List l = parser.getIntegerList("o");
+//		assertEquals(l, {1,2,3,4,5});
+//	}
+//	
+//	@Test
+//	public void NoValueTest() {
+//		parser.add("list", "l", Parser.STRING);
+//		parser.parse("--list");
+//		List l = parser.getIntegerList("o");
+//		assertEquals(l, []);
+//	}
+//	
+//	@Test
+//	public void NonHyphenSeparatorTest() {
+//		parser.add("list1", Parser.STRING);
+//		parser.parse("--list1=\"1,2 4\"");
+//		List l1 = parser.getIntegerList("list1");
+//		parser.parse("--list1=\"1,2 4\"");
+//		List l1 = parser.getIntegerList("list1");
+//		parser.parse("--list1=\"1,2 4\"");
+//		List l1 = parser.getIntegerList("list1");
+//		assertEquals(l, []);
+//	}
 
 }
